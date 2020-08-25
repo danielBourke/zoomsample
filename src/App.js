e5ad3b5eb22a6d8bd45fbbae776e94642473fb0a
Daniel Bourke <reactdb1984@gmail.com>
2020-08-25 11:16:40 +0100

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { ZoomMtg } from "@zoomus/websdk";
-
+import axios from "axios"
 let apiKeys = {
   apiKey: process.env.REACT_APP_ZOOM_API_KEY,
   apiSecret: process.env.REACT_APP_ZOOM_API_SECRET_KEY,
@@ -19,7 +19,7 @@ function App() {
   console.log(apiKeys.apiKey)
   function joinMeeting(signature, meetConfig) {
     ZoomMtg.init({
-      leaveUrl: "https://zoom.us/",
+      leaveUrl: "https://master.d2v2dkbt553b44.amplifyapp.com/",
       isSupportAV: true,
       success: function (success) {
         console.log("Init Success ", success);
@@ -51,19 +51,17 @@ function App() {
      * Signature must be generated on server
      * https://marketplace.zoom.us/docs/sdk/native-sdks/web/essential/signature
      */
-    ZoomMtg.generateSignature({
-      meetingNumber: meetConfig.meetingNumber,
-      apiKey: meetConfig.apiKey,
-      apiSecret: apiKeys.apiSecret,
-      role: meetConfig.role,
-      success: function (res) {
-        console.log("res", res);
+    const generateSignitures = async() => {
+      const  result = await axios.post(`https://rto4pey206.execute-api.us-east-1.amazonaws.com/dev/generatesigniture/0/97704764972`)
+       console.log(JSON.stringify(result))
+      
+      console.log(`The results are: ${result.data.secret}`)
 
-        setTimeout(() => {
-          joinMeeting(res.result, meetConfig);
-        }, 1000);
-      },
-    });
+      setTimeout(() => {
+        joinMeeting(result.data.secret, meetConfig);
+      }, 1000);
+     }
+     generateSignitures()
   }, []);
 
   return <div className="App">Zoom Testing</div>;
